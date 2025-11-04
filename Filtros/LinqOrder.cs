0ce89a662afbbcd4ad0724819a1466b9826a0e9b
Glauber Ferraz <102560669+glaubermferraz@users.noreply.github.com>
2025-11-04 20:47:23 -0300

    Nova classe Ordenar Artistas

---
+++ Filtros/LinqOrder.cs
@@ -0,0 +1,17 @@
+﻿
+using ScreenSoundApi.Modelo;
+
+namespace ScreenSoundApi.Filtros;
+
+internal class LinqOrder
+{
+    public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas)
+    {
+        var artistasOrdenados = musicas.OrderBy(musica => musica.Artista).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine("\nLista de artistas ordenados:");
+        foreach (var artista in artistasOrdenados)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,11 +10,12 @@ using (HttpClient client = new HttpClient())
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         Console.WriteLine("Resposta recebida");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        Console.WriteLine($"Quantidade de músicas: {musicas.Count}");
-        Console.WriteLine("\nMostrando dados da música posição 240 na lista:");
-        musicas[240].ExibirFichaTecnica();
-        Console.WriteLine("\nMostrando todos os gêneros musicais:");
-        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //Console.WriteLine($"Quantidade de músicas: {musicas.Count}");
+        //Console.WriteLine("\nMostrando dados da música posição 240 na lista:");
+        //musicas[240].ExibirFichaTecnica();
+        //Console.WriteLine("\nMostrando todos os gêneros musicais:");
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
     }
     catch (Exception ex)
     {

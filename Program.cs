42c4a8eea2fc242b55ff78eba3400ae409749df0
Glauber Ferraz <102560669+glaubermferraz@users.noreply.github.com>
2025-11-04 19:47:28 -0300

    Adicionar classe Musica e testar Desserializar

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreenSoundApi.Modelo;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
+        Console.WriteLine("Iniciando requisição...");
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        Console.WriteLine("Resposta recebida");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        Console.WriteLine($"Quantidade de músicas: {musicas.Count}\n");
+        musicas[240].ExibirFichaTecnica();
     }
     catch (Exception ex)
     {

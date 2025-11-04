c6ea15941a42be975ad17b8b3f1981b2bae43453
Glauber Ferraz <102560669+glaubermferraz@users.noreply.github.com>
2025-11-04 20:19:15 -0300

    Criando LinqFilter para generos

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using ScreenSoundApi.Modelo;
 using System.Text.Json;
+using ScreenSoundApi.Filtros;
 
 using (HttpClient client = new HttpClient())
 {
@@ -9,8 +10,11 @@ using (HttpClient client = new HttpClient())
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         Console.WriteLine("Resposta recebida");
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-        Console.WriteLine($"Quantidade de músicas: {musicas.Count}\n");
+        Console.WriteLine($"Quantidade de músicas: {musicas.Count}");
+        Console.WriteLine("\nMostrando dados da música posição 240 na lista:");
         musicas[240].ExibirFichaTecnica();
+        Console.WriteLine("\nMostrando todos os gêneros musicais:");
+        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
     }
     catch (Exception ex)
     {

752af5ba4886d7e1111d0aba5e47169a6eece621
Glauber Ferraz <102560669+glaubermferraz@users.noreply.github.com>
2025-11-04 18:41:33 -0300

    Adicionar try catch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿using (HttpClient client = new HttpClient())
 {
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.WriteLine(resposta);
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Tivemos uma exceção: {ex.Message}");
+    }
 }
